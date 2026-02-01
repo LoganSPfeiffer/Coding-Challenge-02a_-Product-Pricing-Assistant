3fbe0e2ce48f533baf3d72da2e6e051a6534b193
Logan Pfeiffer <Lpfeiffer48@usf.edu>
2026-02-01 17:52:04 -0500

    Creating my variables

--- CodingChallenge02a.js
+++ CodingChallenge02a.js
@@ -1,1 +1,10 @@
 console.log("Hello, World!");
+// Product Pricing Assistant
+
+let productName = "Pokemon Card Pack";
+let costPerUnit = 1.99;
+let basePrice = 6.99;
+let discountRate = 0.10; // 10% discount
+const taxRate = 0.07; // 7% sales tax
+const fixedMonthlyCosts = 1000; // Fixed costs per month
+
